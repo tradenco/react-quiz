c882e79043965890f685b29c6630bd5750dfb7f0
Pouet Pouet <frederic@c-azur.com>
2025-09-03 22:06:09 +0200

    init app with json-server and useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 import Header from "./components/Header";
 import Main from "./components/Main.jsx";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status:'loading'
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "DATA_RECEIVED":
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+
+    case 'DATA_ERROR':
+      return {
+        ...state,
+        status: 'error',
+      };
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
 
 function App() {
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({type:'DATA_RECEIVED', payload: data}))
+      .catch(() => dispatch({type:'DATA_ERROR'}));
+  }, [])
   return (
     <div className="app">
       <Header/>
